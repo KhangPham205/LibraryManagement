4f10e29291f9d69ed8c21726ded7709f5971b375
KhangPham205 <ptkhang17122005@gmail.com>
2024-11-19 12:31:32 +0700

    Add map.png

--- WpfApp1/LibraryManagementApplication/obj/Debug/LibraryManagementApplication_Content.g.i.cs
+++ WpfApp1/LibraryManagementApplication/obj/Debug/LibraryManagementApplication_Content.g.i.cs
@@ -8,21 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/angle-left.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/arrow-left.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/cross.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/edit.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/envelope.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/globe.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/home.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/marker.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/plus.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/search.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/settings.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/stats.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/time-past.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/user.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/users-alt.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon/users.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("image/map.png")]
 
 
