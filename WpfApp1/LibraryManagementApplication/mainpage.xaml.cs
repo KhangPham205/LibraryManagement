6136434e5c9ce4c8e93b16d82bf567900c011311
Gnuttttt <dinhthanhtung0312@gmail.com>
2024-11-23 20:08:38 +0700

    mainpage

--- WpfApp1/LibraryManagementApplication/mainpage.xaml.cs
+++ WpfApp1/LibraryManagementApplication/mainpage.xaml.cs
@@ -12,19 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace LibraryManagementApplication
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for mainpage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class mainpage : Page
     {
-        public MainWindow()
+        public mainpage()
         {
             InitializeComponent();
-            Mainpage.Content = new mainpage();
         }
     }
 }
