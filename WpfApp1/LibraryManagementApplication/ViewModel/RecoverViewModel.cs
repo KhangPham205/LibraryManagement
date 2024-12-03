993574e337d40fdeda8b152cef6480799632e066
Gnuttttt <dinhthanhtung0312@gmail.com>
2024-12-03 15:17:15 +0700

    dieu huong ung dung 2

---
+++ WpfApp1/LibraryManagementApplication/ViewModel/RecoverViewModel.cs
@@ -0,0 +1,25 @@
+﻿using LibraryManagementApplication.Model.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+namespace LibraryManagementApplication.ViewModel
+{
+    public class RecoverViewModel : BaseViewModel
+    {
+        public ICommand RecoverCommand { get; set; }
+        public RecoverViewModel()
+        {
+            RecoverCommand = new RelayCommand<UIElement>((p) => true, (p) => { 
+                Window window = Window.GetWindow(p);
+                recover(p);
+                window.Close();
+                });
+        }
+        public void recover(UIElement p) { }//xu ly khoi phuc dung ca 3 thong tin thi tra ra matkhau
+    }
+}
