cf871cf16c5d8e6eebc195cb3538f5880163c76b
KhangPham205 <ptkhang17122005@gmail.com>
2024-12-11 23:03:54 +0700

    Đang sửa đơn mượn-trả

--- WpfApp1/LibraryManagementApplication/addborrow.xaml.cs
+++ WpfApp1/LibraryManagementApplication/addborrow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryManagementApplication.Model.Class;
+using LibraryManagementApplication.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,49 @@ namespace LibraryManagementApplication
     /// </summary>
     public partial class addborrow : Page
     {
-        public addborrow()
-        {
+        private LibraryDbContext _context;
+        private List<BorrowedBook> DanhSachMuon;
+        public addborrow() 
+        { 
             InitializeComponent();
+
+            DanhSachMuon = new List<BorrowedBook>();
+            _context = new LibraryDbContext(); 
+            LoadBookTitles(); 
+        }
+        private void LoadBookTitles() 
+        { 
+            tensachtb.ItemsSource = _context.DauSachs.Select(t => t.TenDauSach).ToList(); 
+        }
+        private void Tensachtb_SelectionChanged(object sender, SelectionChangedEventArgs e) 
+        { 
+            if (tensachtb.SelectedItem != null) 
+            { 
+                string selectedBookTitle = tensachtb.SelectedItem.ToString(); 
+                isbn.ItemsSource = _context.Sachs.Where(t => t.TenDauSach == selectedBookTitle).Select(t => t.ISBN).ToList(); 
+            } 
+        }
+
+        private void addsach_Click(object sender, RoutedEventArgs e)
+        {
+            if (tensachtb.SelectedItem != null && isbn.SelectedItem != null) 
+            {
+                DanhSachMuon.Add(new BorrowedBook 
+                { 
+                    TenDauSach = tensachtb.SelectedItem.ToString(), 
+                    ISBN = isbn.SelectedItem.ToString() 
+                });
+            }
+            sachlist.ItemsSource = null; // Reset the ItemsSource to refresh the UI
+            sachlist.AutoGenerateColumns = false;
+            sachlist.ItemsSource = DanhSachMuon;
         }
     }
+
+    public class BorrowedBook
+    {
+        public string TenDauSach { get; set; }
+        public string ISBN { get; set; }
+    }
+
 }
