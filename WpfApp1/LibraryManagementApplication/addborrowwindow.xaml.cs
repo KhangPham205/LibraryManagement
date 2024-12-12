c25582e3b450c1ebb637fbe56e3f490eb0324a0f
Gnuttttt <dinhthanhtung0312@gmail.com>
2024-12-12 22:16:43 +0700

    tạo addborrow window

---
+++ WpfApp1/LibraryManagementApplication/addborrowwindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using LibraryManagementApplication.ViewModel.ClassViewModel;
+using LibraryManagementApplication.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace LibraryManagementApplication
+{
+    /// <summary>
+    /// Interaction logic for addborrowwindow.xaml
+    /// </summary>
+    public partial class addborrowwindow : Window
+    {
+        private ObservableCollection<ThongTinDonMuon> ttdonmuons;
+        private LibraryDbContext _context;
+        public addborrowwindow()
+        {
+            InitializeComponent();
+            this.DataContext = new DonMuonViewModel();
+            _context = new LibraryDbContext();
+            LoadBookTitles();
+        }
+        private void LoadBookTitles()
+        {
+            tensachtb.ItemsSource = _context.DauSachs.Select(t => t.TenDauSach).ToList();
+            tendg.ItemsSource = _context.DocGias.Select(t => t.MaDG).ToList();
+        }
+        private void Tensachtb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (tensachtb.SelectedItem != null)
+            {
+                string selectedBookTitle = tensachtb.SelectedItem.ToString();
+                isbn.ItemsSource = _context.Sachs.Where(t => t.TenDauSach == selectedBookTitle).Select(t => t.ISBN).ToList();
+            }
+        }
+    }
+}
